9218a58ba36f8424f01cd689eb1586986caac6c7
TeeJayBaker <tom.baker@manchester.ac.uk>
2024-01-11 17:18:37 +0000

    Add Dry masking in reverb function

--- modules/reverb.py
+++ modules/reverb.py
@@ -15,6 +15,7 @@ class Reverb(nn.Module):
         reverb_length: Length of the impulse response. Only used if
             trainable=True.
         add_dry: Add dry signal to reverberated signal on output.
+        impulse_response: Impulse response to use if trainable=False.
         device: Specify whether computed on cpu, cuda or mps
 
     Input: Audio input of size (batch, samples)
@@ -24,6 +25,7 @@ class Reverb(nn.Module):
                 trainable = True,
                 reverb_length = 16000,
                 add_dry = True,
+                impulse_response = None,
                 device = 'mps'):
         
         super(Reverb, self).__init__()
@@ -36,8 +38,36 @@ class Reverb(nn.Module):
             self.impulse_response = nn.Parameter(torch.zeros(self.reverb_length), 
                                                  requires_grad = True)
         else:
-            self.impulse_response = nn.Parameter(torch.zeros(self.reverb_length), 
-                                                 requires_grad = False)
+            if impulse_response is None:
+                raise ValueError('Must provide "ir" tensor if Reverb trainable=False.')
+            self.impulse_response = impulse_response
+
+    def _mask_dry(self, impulse_response):
+        """
+        set first ir to zero to mask dry signal from reverberated signal
+        """
+        # Add batch if 1d
+        if len(impulse_response.shape) == 1:
+            impulse_response = impulse_response.unsqueeze(0)
+        # delete channel dimensions if 3d
+        if len(impulse_response.shape) == 3:
+            impulse_response = impulse_response.squeeze(1)
+
+        dry_mask = torch.zeros([int(impulse_response.shape[0]), 1], dtype = torch.FloatTensor)
+        return torch.cat([dry_mask, impulse_response[:, 1:]], dim=1)
+    
+    def _match_dimensions(self, audio, impulse_response):
+        """
+        Match dimensions of audio and impulse response via batch size
+        """
+        raise NotImplementedError
             
     def forward(self, audio):
+        """
+        Apply impulse response to audio
+        """
+        audio = torch.FloatTensor(audio)
+        self.impulse_response = torch.FloatTensor(self.impulse_response)
+        self.impulse_response = self.impulse_response.to(self.device)
+
         raise NotImplementedError
