8eb4db5aeffc9131fde064c11aded9275d71991b
TeeJayBaker <tom.baker@manchester.ac.uk>
2024-01-31 17:26:44 +0000

    added midi_to_hz to util function

--- basic_pitch/utils.py
+++ basic_pitch/utils.py
@@ -9,3 +9,9 @@ def hz_to_midi(frequency: float) -> float:
     Convert a frequency in Hz to a MIDI note number
     """
     return 69 + 12 * np.log2(frequency / 440.0)
+
+def midi_to_hz(midi_note: int) -> float:
+    """
+    Convert a MIDI note number to a frequency in Hz
+    """
+    return 440.0 * 2 ** ((midi_note - 69) / 12)
