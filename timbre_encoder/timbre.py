34f629ae5cdf25a23219b956982273d7c5cb3ce2
TeeJayBaker <tom.baker@manchester.ac.uk>
2024-02-07 16:30:44 +0000

    Begun source separation

---
+++ timbre_encoder/timbre.py
@@ -0,0 +1,16 @@
+"""
+timbre encoder, that takes in pitch and spectrogram 
+and returns a source separated spectrogram and MFCCs
+"""
+
+import torch
+import torchaudio
+import torch.nn as nn
+
+
+class TimbreEncoder(nn.module):
+    def __init__(self):
+        super(TimbreEncoder, self).__init__()
+        self.pitch_encoder = nn.Sequential(
+            nn.Linear(1)
+        )
