88e0ac0b704ffabc625089e6577b09060f4933f8
TeeJayBaker <tom.baker@manchester.ac.uk>
2024-03-20 16:54:08 +0000

    Constructed autoencoder and cropped audio output to match input

--- autoencoder.py
+++ autoencoder.py
@@ -19,6 +19,32 @@ class AutoEncoder(nn.Module):
     """
     Autoencoder for the AE architecture
     """
+
+    def __init__(self, 
+                 use_z: bool = False, 
+                 mlp_hidden_dims: int = 512,
+                 reverb_length: int = 16000,
+                 add_dry: bool = True,
+                 impulse_response: bool = None,
+                 device: str = 'mps'):
+        super(AutoEncoder, self).__init__()
+
+        self.encoder = Encoder()
+        self.decoder = Decoder(use_z=use_z, mlp_hidden_dims=mlp_hidden_dims)
+        self.reverb = Reverb(reverb_length=reverb_length, add_dry=add_dry, impulse_response=impulse_response, device=device)
+        self.noise = FilteredNoise()
+        self.synth = AdditiveSynth()
+
+    def forward(self, x):
+        parts = self.encoder(x)
+        output = self.decoder(parts)
+        audio = self.synth(output)
+        noise = self.noise(output['noise'])
+        reverbed = self.reverb(audio + noise)
+        audio = audio[:, :x.shape[1]]
+        noise = noise[:, :x.shape[1]]
+        reverbed = reverbed[:, :x.shape[1]]
+        return audio, noise, reverbed
     
 
 import librosa
@@ -27,24 +53,6 @@ audio = librosa.load("pitch_encoder/01_BN2-131-B_solo_mic.wav", sr=22050, durati
 print(audio.shape)
 audio = torch.tensor(audio).unsqueeze(0).to('cpu')
 
-encoder = Encoder()
-decoder = Decoder()
-parts = encoder(audio)
-output = decoder(parts)
-print(output['pitch'].shape)
-print(output['amplitude'].shape)
-print(output['harmonics'].shape)
-print(output['noise'].shape)
-
-synth = AdditiveSynth()
-audio = synth(output)
-print(audio.shape)
-
-noise = FilteredNoise()
-noise = noise(output['noise'])
-print(noise.shape)
-
-reverb = Reverb()
-print((audio+noise).shape)
-audio = reverb(audio + noise)
-print(audio.shape)
+model = AutoEncoder()
+audio, noise, reverbed = model(audio)
+print(audio.shape, noise.shape, reverbed.shape)
