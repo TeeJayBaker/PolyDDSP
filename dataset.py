b9f78015190f9b2d6cc735e95074bd54131e15f2
TeeJayBaker <tom.baker@manchester.ac.uk>
2024-03-25 18:05:40 +0000

    Create AudioDataset class

---
+++ dataset.py
@@ -0,0 +1,45 @@
+"""
+Dataset class for loading audio data
+"""
+
+import torchaudio
+from torch.utils.data import Dataset
+import numpy as np
+
+class AudioDataset(Dataset):
+    """
+    Dataset class for loading audio data
+
+    Args:
+        paths: paths to audio files
+        sr: sample rate of audio
+        duration: duration of audio
+        random: whether to random sample audio
+    """
+    def __init__(self, paths: list[str], 
+                 sr: int = 22050, 
+                 duration: float = 10.0, 
+                 random: bool = True):
+        super(AudioDataset, self).__init__()
+
+        self.paths = paths
+        self.sr = sr
+        self.duration = duration
+        self.random = random
+        
+    def __len__(self):
+        return len(self.paths)
+
+    def __getitem__(self, idx: int):
+        audio, old_sr = torchaudio.load(self.paths[idx], normalize=True)
+        audio = audio[0]
+        audio = audio.unsqueeze(0)
+        audio = torchaudio.transforms.Resample(orig_freq=old_sr, 
+                                               new_freq=self.sr)(audio)
+        if self.random:
+            start = self.rng.integers(0, audio.shape[-1] - self.duration * self.sr)
+            audio = audio[:, start:start + int(self.duration * self.sr)]
+        else: 
+            audio = audio[:, :int(self.duration * self.sr)]
+        
+        return audio    
