dec84a494ea3c794d43e414d9682fe08cb64e34a
TeeJayBaker <tom.baker@manchester.ac.uk>
2024-04-17 17:28:30 +0100

    Implement a tf.signal.frame version in torch

--- pitch_encoder/utils.py
+++ pitch_encoder/utils.py
@@ -4,6 +4,7 @@ Utility functions for basic_pitch
 
 import numpy as np
 import torch
+import torch.nn.functional as F
 
 
 def hz_to_midi(frequency: float) -> float:
@@ -36,3 +37,23 @@ def unravel_index(index: int, shape: tuple) -> tuple:
         unravelled.append(index % dim)
         index = index // dim
     return tuple(reversed(unravelled))
+
+
+def frame(signal, frame_length, frame_step, pad_end=False, pad_value=0, axis=-1):
+    """
+    equivalent of tf.signal.frame
+    """
+    signal_length = signal.shape[axis]
+    pad_size = 0
+    if pad_end:
+        frames_overlap = frame_length - frame_step
+        rest_samples = np.abs(signal_length - frames_overlap) % np.abs(
+            frame_length - frames_overlap
+        )
+        pad_size = int(frame_length - rest_samples)
+        if pad_size != 0:
+            pad_axis = [0] * signal.ndim
+            pad_axis[axis] = pad_size
+            signal = F.pad(signal, pad_axis, "constant", pad_value)
+    frames = signal.unfold(axis, frame_length, frame_step)
+    return frames, pad_size
